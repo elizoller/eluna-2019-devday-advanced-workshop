cc7c6053b851bceb63e30412947aa7f988e05209
Eli Zoller <zoller.eli@gmail.com>
2019-04-29 16:46:48 -0400

    add submenu tesT

--- e2e-cypress/cypress/integration/NYU/submenu.spec.js
+++ e2e-cypress/cypress/integration/NYU/submenu.spec.js
@@ -1,12 +1,20 @@
 describe('searchbar-submenu', () => {
   before(() => {
-    cy.visit('/search?vid=NYU');
+    cy.visit('/search?vid=NYU', {
+      onBeforeLoad(win) {
+        cy.stub(win, 'open').as('windowOpen')
+      }
+    });
   })
 
   describe('submenu', () => {
-    it('exists')
+    it('exists', () => {
+      cy.get('search-bar-sub-menu').should('exist')
+    })
 
-    it('is visible')
+    it('is visible', () => {
+      cy.get('.search-bar-sub-menu').should('be.visible');
+    })
 
     describe('has the correct menu items', () => {
       const submenuItems = [
@@ -20,10 +28,28 @@ describe('searchbar-submenu', () => {
         }
       ]
 
-      it(`has ${submenuItems.length} buttons`)
+      it(`has ${submenuItems.length} buttons`, () => {
+        cy.get('.search-bar-sub-menu-item').should('have.length', submenuItems.length)
+      })
 
       submenuItems.forEach(({ label, link }, idx) => {
-        it(`has a button with ${label} which opens ${link} when clicked`)
+        it(`has a button with ${label} which opens ${link} when clicked`, () => {
+          cy.window().then(win => {
+            const spy = cy.stub(win, 'open')
+
+            cy.get('.search-bar-sub-menu-item').contains(label).click().then($el => {
+              expect(spy).to.be.calledWith(link);
+            })
+            // OR BELOW, idx is like the id
+            cy.get('search-bar-sub-menu button')
+              .eq(idx)
+              .should('contain', label)
+              .click('center')
+              .then(() => {
+                expect(spy).to.be.calledWith(link)
+              })
+          })
+        })
       })
     })
   })
