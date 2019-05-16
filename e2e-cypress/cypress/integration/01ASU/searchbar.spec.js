9d8df2e6cef43dd5921dcaaf156112579dc506f2
Eli Zoller <zoller.eli@gmail.com>
2019-05-16 15:51:28 -0700

    test that external facet links are present and contain the search term

--- e2e-cypress/cypress/integration/01ASU/searchbar.spec.js
+++ e2e-cypress/cypress/integration/01ASU/searchbar.spec.js
@@ -75,8 +75,17 @@ describe('asu customizations', () => {
           expect($el.text().length).to.be.at.least(1)
         });
       cy.get('#facets').should('exist');
-      cy.get('.available-facets').should('have.length', 14)
+      cy.get('.available-facets').should('have.length', 13)
       cy.get('h3.section-title-header').contains('External Search').should('exist');
+      cy.get('div[data-facet-group="External Search"] .md-chips .md-chip').should('have.length', 2);
+    })
+
+    it(`should have a worldcat link with the search term in it`, () => {
+      cy.get('div[data-facet-group="External Search"] .md-chips .md-chip a').first().should('have.attr', 'href').and('equal', 'https://www.worldcat.org/search?qt=owc_search&q=kw:(science)');
+    })
+
+    it(`should have a google scholar link with the search term in it`, () => {
+      cy.get('div[data-facet-group="External Search"] .md-chips .md-chip a').eq(1).should('have.attr', 'href').and('equal', 'https://scholar.google.com/scholar?q=science');
     })
   });
 
