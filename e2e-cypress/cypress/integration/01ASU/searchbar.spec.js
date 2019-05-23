0d8945641b5c38545717c09e105bb6f885c00608
Eli Zoller <zoller.eli@gmail.com>
2019-05-23 11:08:31 -0700

    add twilio sms integration tests

--- e2e-cypress/cypress/integration/01ASU/searchbar.spec.js
+++ e2e-cypress/cypress/integration/01ASU/searchbar.spec.js
@@ -89,6 +89,44 @@ describe('asu customizations', () => {
     })
   });
 
+  describe('sms call number/twilio integration', () => {
+    before(() => {
+      cy.get('button[aria-label="Clear search text"]').click()
+      cy.get('#searchBar')
+        .type('frogs{enter}')
+      cy.url().should('include', 'frogs')
+    });
 
+    it('should have a twilio button in the brief results', () => {
+      cy.get(`[id^='SEARCH_RESULT_RECORDID_']`)
+        .first()
+        .then($el => {
+          expect($el.text().length).to.be.at.least(1)
+        });
+      cy.get(`#facets`).click({force: true})
+      cy.get(`[id ^= 'SEARCH_RESULT_RECORDID_']`)
+        .first()
+        .get(`#briefResultMoreOptionsButton`)
+        .click();
+      cy.get(`prm-action-list .button-text`).should('contain', 'Text Me');
+    });
+
+    it('should have a twilio button in the full results', () => {
+      cy.get(`[id ^= 'SEARCH_RESULT_RECORDID_']:first .item-title`)
+        .click();
+      cy.get(`prm-full-view prm-action-list .button-text`).should('contain', 'Text Me');
+    });
+
+    it('should open the form when clicked', () => {
+      cy.get(`prm-full-view prm-action-list li[name="send_sms"]`).click();
+      cy.get(`twilio-send-sms`).should('exist');
+      cy.get(`twilio-send-sms input[name="phoneNumber"]`).should('exist');
+    });
+
+    // can't actually test that it sends because captcha is enabled HAH
+    after(() => {
+      cy.get(`.md-dialog-container > .layout-row > button[aria-label="Close Full Display"]`).click();
+    });
+  });
 
 })
